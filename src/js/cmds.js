ceb5ea71f64d9c57b33bad7a9c751a1c372276cb
Bruno Cheles <bruno.cheles@outlook.com>
2023-09-12 02:25:21 -0300

    Updates dos JS
    Update principalmente dos arquivos JS.

--- src/js/cmds.js
+++ src/js/cmds.js
@@ -15,3 +15,49 @@ function showTextProj(element) {
 function hideTextProj(element) {
     element[0].classList.add("hidden");
 }
+
+function actualPage() {
+    let url_atual = window.location.href;
+    let url_div = url_atual.split("/");
+    let page = url_div.pop();
+
+    let menu_itens = document.getElementsByClassName("menu-btn");
+
+    switch (page) {
+        case "about.html":
+            for(item of menu_itens) {
+                item.classList.remove("page-activated");
+            }
+            menu_itens[1].classList.add("page-activated");
+            break;
+        case "projects.html":
+            for(item of menu_itens) {
+                item.classList.remove("page-activated");
+            }
+            menu_itens[2].classList.add("page-activated");
+            break;
+        case "contact.html":
+            for(item of menu_itens) {
+                item.classList.remove("page-activated");
+            }
+            menu_itens[3].classList.add("page-activated");
+            break;
+        default:
+            for(item of menu_itens) {
+                item.classList.remove("page-activated");
+            }
+            menu_itens[0].classList.add("page-activated");
+            break;
+    }
+}
+
+function preLoad() {
+    let pre_load = document.getElementById("preloader");
+    pre_load.innerHTML = (`
+        <div id="status">&nbsp;</div>
+        <div class="loader">
+            <span class="loader-pin"></span>
+            <span class="loader-title">Carregando...</span>
+        </div>
+    `);
+}
